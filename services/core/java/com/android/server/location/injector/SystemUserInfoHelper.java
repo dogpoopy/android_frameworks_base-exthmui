LMODroid/platform_frameworks_base

commit e99d00cc7c867ae86ee2a52d997e098e42c54660

services: location: Whitelist parallel space users
Fixes unable to use location in parallel spaces.

Change-Id: Ibf2c7dcd41bd10fafd22c23b9ea6704b10e0ad8e
Signed-off-by: LibXZR <i@xzr.moe>
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

services/core/java/com/android/server/location/injector/SystemUserInfoHelper.java
@@ -33,8 +33,11 @@ import android.util.IndentingPrintWriter;
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.util.Preconditions;
 import com.android.server.LocalServices;
+import com.android.server.libremobileos.ParallelSpaceManagerService;
 
 import java.io.FileDescriptor;
+import java.lang.Integer;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -111,7 +114,8 @@ public class SystemUserInfoHelper extends UserInfoHelper {
         if (activityManagerInternal != null) {
             final long identity = Binder.clearCallingIdentity();
             try {
-                return activityManagerInternal.isCurrentProfile(userId);
+                return activityManagerInternal.isCurrentProfile(userId) ||
+                        ParallelSpaceManagerService.isCurrentParallelUser(userId);
             } finally {
                 Binder.restoreCallingIdentity(identity);
             }
@@ -144,7 +148,10 @@ public class SystemUserInfoHelper extends UserInfoHelper {
 
         final long identity = Binder.clearCallingIdentity();
         try {
-            return userManager.getEnabledProfileIds(userId);
+            ArrayList profiles = new ArrayList<>(
+                    Arrays.asList(userManager.getEnabledProfileIds(userId)));
+            profiles.addAll(ParallelSpaceManagerService.getCurrentParallelUserIds());
+            return profiles.stream().mapToInt(i -> ((Integer) i).intValue()).toArray();
         } finally {
             Binder.restoreCallingIdentity(identity);
         }

dogpoopy/android_frameworks_base-exthmui

commit eaf02528e34f33be1dd38e493a679b112b08ae92

SystemUserInfoHelper: ParallelSpace: Fix crash when switching user
E SystemServiceManager: SystemService failure: Failure reporting Switch of user 25 to service com.android.server.location.LocationManagerService$Lifecycle
E SystemServiceManager: java.lang.ClassCastException: int[] cannot be cast to java.lang.Integer

Fixes: e99d00cc7c867ae86ee2a52d997e098e42c54660 ("services: location: Whitelist parallel space users")

services/core/java/com/android/server/location/injector/SystemUserInfoHelper.java
@@ -36,8 +36,6 @@ import com.android.server.LocalServices;
 import com.android.server.exthmui.ParallelSpaceManagerService;
 
 import java.io.FileDescriptor;
-import java.lang.Integer;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -148,10 +146,14 @@ public class SystemUserInfoHelper extends UserInfoHelper {
 
         final long identity = Binder.clearCallingIdentity();
         try {
-            ArrayList profiles = new ArrayList<>(
-                    Arrays.asList(userManager.getEnabledProfileIds(userId)));
-            profiles.addAll(ParallelSpaceManagerService.getCurrentParallelUserIds());
-            return profiles.stream().mapToInt(i -> ((Integer) i).intValue()).toArray();
+            int[] profileIds = userManager.getEnabledProfileIds(userId);
+            int[] parallelUserIds = ParallelSpaceManagerService.getCurrentParallelUserIds()
+                    .stream().mapToInt(i -> i).toArray();
+            int[] combinedIds = new int[profileIds.length + parallelUserIds.length];
+            System.arraycopy(profileIds, 0, combinedIds, 0, profileIds.length);
+            System.arraycopy(parallelUserIds, 0, combinedIds, profileIds.length,
+                    parallelUserIds.length);
+            return combinedIds;
         } finally {
             Binder.restoreCallingIdentity(identity);
         }